8e3846b9f01a38c41f4b9d897c99cb0608d269fc
Midhunkumar-se <midhu025@gmail.com>
2024-05-09 19:16:03 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import BadRequestError from "../errors/bad-request.js";
+import UnauthenticatedError from "../errors/unauthenticated.js";
 import User from "../models/user.model.js";
 
 export const signup = async (req, res, next) => {
@@ -18,6 +19,39 @@ export const signup = async (req, res, next) => {
 
     await User.create({ ...req.body });
     res.json({ message: "Signup successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      throw new BadRequestError("Please provide email and password");
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
+
+    const token = user.createJWT();
+
+    const { password: pass, ...rest } = user._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -27,6 +28,21 @@ userSchema.pre("save", async function () {
   const salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
 });
+
+userSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+userSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcryptjs.compare(canditatePassword, this.password);
+  return isMatch;
+};
 
 const User = mongoose.model("User", userSchema);
 
