75bc9e4dde1074187e521932d72cfb026e37a29f
Midhunkumar-se <midhu025@gmail.com>
2024-05-09 18:38:55 +0530

    create the footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,89 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap font-semibold dark:text-white"
+            >
+              <span className="font-bold text-base sm:text-xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                Midhun
+              </span>
+              <span className="text-[10px]">.Blog</span>
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://midhunkumar.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Midhun's Portfolio
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Midhun's Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/Midhunkumar-se"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Midhun.blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon
+              href="https://github.com/Midhunkumar-se"
+              icon={BsGithub}
+            />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
