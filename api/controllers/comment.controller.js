d3d18b7432b9d217a17d45d0b0175f883ce14321
Midhunkumar-se <midhu025@gmail.com>
2024-05-24 11:34:25 +0530

    add edit functionality to the comment component

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -58,3 +58,28 @@ export const likeComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+
+    if (!comment) {
+      throw new NotFoundError("Comment not found");
+    }
+
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      throw new ForbiddenError("You are not allowed to edit this comment");
+    }
+
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true }
+    );
+    res.status(200).json(editedComment);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -85,6 +85,15 @@ function CommentSection({ postId }) {
       console.log(error.message);
     }
   };
+
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
+
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -153,6 +162,7 @@ function CommentSection({ postId }) {
                 key={comment._id}
                 comment={comment}
                 onLike={handleLike}
+                onEdit={handleEdit}
               />
             );
           })}
