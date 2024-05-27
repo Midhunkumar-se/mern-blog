14e1747375b5c895a3713f9384856b3b21c87c85
Midhunkumar-se <midhu025@gmail.com>
2024-05-27 18:59:07 +0530

    complete about and projects page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,10 @@
-function Projects() {
-  return <div>Projects</div>;
-}
+import CallToAction from "../components/CallToAction";
 
-export default Projects;
+export default function Projects() {
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <CallToAction />
+    </div>
+  );
+}
